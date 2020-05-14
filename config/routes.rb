64925d08c08fc81f00911d1aa45122422d031b3d
nekat0902 <shinehuman@gmail.com>
2020-05-15 05:11:11 +0900

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    root 'inquiry#index' 
     get 'inquiry' => 'inquiry#index'              # 入力画面
     post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
     post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
